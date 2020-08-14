ba1c528b61a8554082e39591f69e3a5f6dfa602d
Kasper Bogebjerg Pedersen <kasperbp@gmail.com>
2020-08-14 15:01:06 +0200

    its hip to be square

--- 8queen/api/lib.js
+++ 8queen/api/lib.js
@@ -4,7 +4,6 @@ const proxy = require('express-http-proxy');
 module.exports = proxy('daily.clm-dev.cloud', {
   https: true,
   proxyReqOptDecorator: async function (proxyReqOpts, srcReq) {
-    console.log(process.env.API_KEY.length, process.env.API_KEY[0]);
     proxyReqOpts.headers['Authorization'] = 'ApiKey ' + process.env.API_KEY;
     return proxyReqOpts;
   },
